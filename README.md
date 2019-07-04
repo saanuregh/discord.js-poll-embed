2321c2b66b9ff744e9613f50fd3957cd0f6a0791
saanuregh <saanu09xs@gmail.com>
2019-07-05 02:16:13 +0530

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
+<div align="center">
+  <p>
+    <a href="https://nodei.co/npm/discord.js-poll-embed
+/"><img src="https://nodei.co/npm/discord.js-poll-embed.png?downloads=true&stars=true" alt="NPM info" /></a>
+  </p>
+</div>
+
+
 # discord.js-poll-embed
-A simple utility to create polls with just embeds and emoji reactions.
+A simple utility to create polls with just embeds and emoji reactions. No poll API required, just Discord.js reactions. By default supports following functionalities,
+* By default supports 10 poll options (can be increased).
+* Timed poll.
+* Force closing poll.
+* Custom emojis for voting (Required for bypassing default options limit)
+
+Supports only discord.js@^12.0.0 (master).
+# Installation
+* `npm install discord.js-poll-embed`
+
+# Usage
+__Basic Bot Example__
+```js
+// Import the discord.js-pagination package
+const pollEmbed = require('discord.js-poll-embed');
+
+// Call the pollEmbed method, first three arguments are required
+// title is the poll title
+// options is an array of strings, which contains the poll options
+// timeout is the time in seconds for which users can vote for. 0 makes it infinite and default value is 30 seconds
+// emojiList is the list of emojis used for voting. Defaults to 10 simple digit emojis. Which also limits the no of options you can give by default to 10. While using custom emojis be careful that discord doesnt support some emojis.
+// forceEndPollEmoji is the emoji which can be voted by the poll author to force close voting. Default value is a green check box.
+pollEmbed(msg, title, options, timeout, emojiList, forceEndPollEmoji);
+// There you go, now you have poll embeds
+```
+# Preview
---
+++ index.js
@@ -0,0 +1,78 @@
+const { MessageEmbed } = require('discord.js');
+
+const defEmojiList = [
+	'\u0031\u20E3',
+	'\u0032\u20E3',
+	'\u0033\u20E3',
+	'\u0034\u20E3',
+	'\u0035\u20E3',
+	'\u0036\u20E3',
+	'\u0037\u20E3',
+	'\u0038\u20E3',
+	'\u0039\u20E3',
+	'\uD83D\uDD1F'
+];
+
+const pollEmbed = async (msg, title, options, timeout = 30, emojiList = defEmojiList, forceEndPollEmoji = '\u2705') => {
+	if (!msg && !msg.channel) return msg.reply('Channel is inaccessible.');
+	if (!title) return msg.reply('Poll title is not given.');
+	if (!options) return msg.reply('Poll options are not given.');
+	if (options.length < 2) return msg.reply('Please provide more than one choice.');
+	if (options.length <= emojiList.length) return msg.reply(`Please provide ${emojiList.length} or less choices.`);
+
+	let text = `*To vote, react using the correspoding emoji.\nThe voting will end in **${timeout} seconds**.\nPoll creater can end the poll **forcefully** by reacting to ${forceEndPollEmoji} emoji.*\n\n`;
+	const emojiInfo = {};
+	for (const option of options) {
+		const emoji = emojiList.splice(0, 1);
+		emojiInfo[emoji] = { option: option, votes: 0 };
+		text += `${emoji} : \`${option}\`\n\n`;
+	}
+	const usedEmojis = Object.keys(emojiInfo);
+	usedEmojis.push(forceEndPollEmoji);
+
+	const poll = await msg.channel.send(embedBuilder(title, msg.author.id).setDescription(text));
+	for (const emoji of usedEmojis) await poll.react(emoji);
+
+	const reactionCollector = poll.createReactionCollector(
+		(reaction, user) => usedEmojis.includes(reaction.emoji.name) && user.id === msg.author.id,
+		timeout === 0 ? {} : { time: timeout * 1000 }
+	);
+	const voterInfo = new Map();
+	reactionCollector.on('collect', (reaction, user) => {
+		if (usedEmojis.includes(reaction.emoji.name)) {
+			if (reaction.emoji.name === forceEndPollEmoji && msg.author.id === user.id) reactionCollector.stop();
+			if (!voterInfo.has(user.id)) voterInfo.set(user.id, { emoji: reaction.emoji.name });
+			const votedEmoji = voterInfo.get(user.id).emoji;
+			if (votedEmoji !== reaction.emoji.name) {
+				const lastVote = poll.reactions.get(votedEmoji);
+				lastVote.count -= 1;
+				lastVote.users.remove(user.id);
+				emojiInfo[votedEmoji].votes -= 1;
+				voterInfo.set(user.id, { emoji: reaction.emoji.name });
+			}
+			emojiInfo[reaction.emoji.name].votes += 1;
+		}
+	});
+
+	reactionCollector.on('dispose', (reaction, user) => {
+		if (usedEmojis.includes(reaction.emoji.name)) {
+			voterInfo.delete(user.id);
+			emojiInfo[reaction.emoji.name].votes -= 1;
+		}
+	});
+
+	reactionCollector.on('end', () => {
+		text = '*Ding! Ding! Ding! Time\'s up!\n Results are in,*\n\n';
+		for (const emoji in emojiInfo) text += `\`${emojiInfo[emoji].option}\` - \`${emojiInfo[emoji].votes}\`\n\n`;
+		poll.delete();
+		msg.channel.send(embedBuilder(title, msg.author.id).setDescription(text));
+	});
+};
+
+const embedBuilder = (title, authorId) => {
+	return new MessageEmbed()
+		.setAuthor(`Poll - ${title}`, this.client.user.displayAvatarURL())
+		.setFooter(`Poll created by <@${authorId}>`);
+};
+
+module.exports = pollEmbed;
